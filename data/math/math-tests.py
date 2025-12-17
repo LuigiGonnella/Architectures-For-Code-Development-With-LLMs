a695bec98b72c494a2e314a0ff4d6b5ce40dd01f
fpgmina <francesco.mina1986@gmail.com>
2025-12-17 22:01:44 +0100

    appease black

--- data/math/math-tests.py
+++ data/math/math-tests.py
@@ -5,26 +5,30 @@ import unittest
 
 # ===== HumanEval/39 (prime_fib) =====
 
-def prime_fib(n):
-        import math
 
-        def is_prime(p):
-            if p < 2:
+def prime_fib(n):
+    import math
+
+    def is_prime(p):
+        if p < 2:
+            return False
+        for k in range(2, min(int(math.sqrt(p)) + 1, p - 1)):
+            if p % k == 0:
                 return False
-            for k in range(2, min(int(math.sqrt(p)) + 1, p - 1)):
-                if p % k == 0:
-                    return False
-            return True
-        f = [0, 1]
-        while True:
-            f.append(f[-1] + f[-2])
-            if is_prime(f[-1]):
-                n -= 1
-            if n == 0:
-                return f[-1]
+        return True
+
+    f = [0, 1]
+    while True:
+        f.append(f[-1] + f[-2])
+        if is_prime(f[-1]):
+            n -= 1
+        if n == 0:
+            return f[-1]
+
 
 class TestPrime_fib(unittest.TestCase):
     """Tests for prime_fib"""
+
     def test_case_1(self):
         self.assertEqual(prime_fib(1), 2)
 
@@ -56,17 +60,19 @@ class TestPrime_fib(unittest.TestCase):
         self.assertEqual(prime_fib(10), 433494437)
 
 
-
 # ===== HumanEval/49 (modp) =====
 
+
 def modp(n, p):
-        ret = 1
-        for i in range(n):
-            ret = (2 * ret) % p
-        return ret
+    ret = 1
+    for i in range(n):
+        ret = (2 * ret) % p
+    return ret
+
 
 class TestModp(unittest.TestCase):
-    """Tests for modp"""   
+    """Tests for modp"""
+
     def test_case_1(self):
         self.assertEqual(modp(3, 5), 3)
 
@@ -89,25 +95,28 @@ class TestModp(unittest.TestCase):
         self.assertEqual(modp(31, 5), 3)
 
 
-
 # ===== HumanEval/59 (largest_prime_factor) =====
 
+
 def largest_prime_factor(n):
-        def is_prime(k):
-            if k < 2:
+    def is_prime(k):
+        if k < 2:
+            return False
+        for i in range(2, k - 1):
+            if k % i == 0:
                 return False
-            for i in range(2, k - 1):
-                if k % i == 0:
-                    return False
-            return True
-        largest = 1
-        for j in range(2, n + 1):
-            if n % j == 0 and is_prime(j):
-                largest = max(largest, j)
-        return largest
+        return True
+
+    largest = 1
+    for j in range(2, n + 1):
+        if n % j == 0 and is_prime(j):
+            largest = max(largest, j)
+    return largest
+
 
 class TestLargest_prime_factor(unittest.TestCase):
-    """Tests for largest_prime_factor"""   
+    """Tests for largest_prime_factor"""
+
     def test_case_1(self):
         self.assertEqual(largest_prime_factor(15), 5)
 
@@ -124,14 +133,16 @@ class TestLargest_prime_factor(unittest.TestCase):
         self.assertEqual(largest_prime_factor(13195), 29)
 
 
-
 # ===== HumanEval/60 (sum_to_n) =====
 
+
 def sum_to_n(n):
-        return sum(range(n + 1))
+    return sum(range(n + 1))
+
 
 class TestSum_to_n(unittest.TestCase):
-    """Tests for sum_to_n"""    
+    """Tests for sum_to_n"""
+
     def test_case_1(self):
         self.assertEqual(sum_to_n(1), 1)
 
@@ -148,27 +159,33 @@ class TestSum_to_n(unittest.TestCase):
         self.assertEqual(sum_to_n(100), 5050)
 
 
-
 # ===== HumanEval/75 (is_multiply_prime) =====
 
-def is_multiply_prime(a):
-        def is_prime(n):
-            for j in range(2,n):
-                if n%j == 0:
-                    return False
-            return True
 
-        for i in range(2,101):
-            if not is_prime(i): continue
-            for j in range(2,101):
-                if not is_prime(j): continue
-                for k in range(2,101):
-                    if not is_prime(k): continue
-                    if i*j*k == a: return True
-        return False
+def is_multiply_prime(a):
+    def is_prime(n):
+        for j in range(2, n):
+            if n % j == 0:
+                return False
+        return True
+
+    for i in range(2, 101):
+        if not is_prime(i):
+            continue
+        for j in range(2, 101):
+            if not is_prime(j):
+                continue
+            for k in range(2, 101):
+                if not is_prime(k):
+                    continue
+                if i * j * k == a:
+                    return True
+    return False
+
 
 class TestIs_multiply_prime(unittest.TestCase):
-    """Tests for is_multiply_prime"""   
+    """Tests for is_multiply_prime"""
+
     def test_case_1(self):
         self.assertEqual(is_multiply_prime(5), False)
 
@@ -200,6 +217,5 @@ class TestIs_multiply_prime(unittest.TestCase):
         self.assertEqual(is_multiply_prime(11 * 13 * 7), True)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
